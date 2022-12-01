8b433fa889fe33107f61ab9d33d075000d4ce4c0
Pedro Cunha <phcs-12@hotmail.com>
2022-12-01 09:36:58 -0300

    inital commit

---
+++ capliza/src/main/java/com/grupo5/capliza/CaplizaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.capliza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaplizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaplizaApplication.class, args);
+	}
+
+}
---
+++ capliza/src/test/java/com/grupo5/capliza/CaplizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.capliza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaplizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
